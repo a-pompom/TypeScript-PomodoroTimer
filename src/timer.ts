b6a448cf0a8fccadbcdbc4d840708f8c6603c10a
a-pompom <43694794+a-pompom@users.noreply.github.com>
2022-07-03 18:54:42 +0900

    [#1] 画面表示用に残り時間を「15:00」のような形式で整形する処理を作成
    残り時間そのものを表現するオブジェクトをつくる処理が組めたので、本来の目的である
    経過時間・全体の時間から画面へ表示する残り時間の文字列を出力する処理をつくった

--- src/timer.ts
+++ src/timer.ts
@@ -5,6 +5,7 @@ export type RestTime = {
 }
 // 残りの時間を表現するオブジェクトを生成
 type GenerateRestTime = (totalMs: number, elapsedMs: number) => RestTime;
+type ToRestTimeString = (rest: RestTime) => string;
 
 const SECOND_MS = 1000;
 const MINUTE_MS = 60 * 1000;
@@ -14,6 +15,8 @@ const MINUTE_MS = 60 * 1000;
  *
  * @param totalMs タイマーでカウントする全体の時間(ミリ秒)
  * @param elapsedMs 経過時間(ミリ秒)
+ *
+ * @return 残り時間の分・秒部分を表現するオブジェクト
  */
 export const generateRestTime: GenerateRestTime = (totalMs, elapsedMs) => {
     const rest = totalMs - elapsedMs;
@@ -28,3 +31,17 @@ export const generateRestTime: GenerateRestTime = (totalMs, elapsedMs) => {
     };
 };
 
+/**
+ * 画面へ表示する残り時間文字列を取得
+ *
+ * @param rest 残り時間の分数・秒数部分を持つオブジェクト
+ * @return 00:30, 20:31のような、残り時間を表現する文字列
+ */
+export const toRestTimeString: ToRestTimeString = (rest) => {
+    const TIME_PART_LENGTH = 2;
+    // 00, 51のような表示用フォーマットへ整形
+    const minutePart = rest.minute.toString().padStart(TIME_PART_LENGTH, '0');
+    const secondPart = rest.second.toString().padStart(TIME_PART_LENGTH, '0');
+
+    return `${minutePart}:${secondPart}`;
+};
--- tests/timer.test.ts
+++ tests/timer.test.ts
@@ -1,4 +1,4 @@
-import {generateRestTime, RestTime} from 'src/timer';
+import {generateRestTime, RestTime, toRestTimeString} from 'src/timer';
 
 describe('時刻オブジェクト生成処理', () => {
 
@@ -13,6 +13,9 @@ describe('時刻オブジェクト生成処理', () => {
             60 * 1000 * 25, 180 * 1000, {minute: 22, second: 0},
         ],
         [
+            60 * 1000 * 25, 60 * 24 * 1000, {minute: 1, second: 0},
+        ],
+        [
             60 * 1000 * 25, (601 * 1000 - 1), {minute: 14, second: 59},
         ]
     ])('分・秒それぞれを持つ時刻オブジェクトがつくられるか: %i, %i, %p', (totalMs: number, elapsedMs: number, expected: RestTime) => {
@@ -22,5 +25,27 @@ describe('時刻オブジェクト生成処理', () => {
         const actual = sut(totalMs, elapsedMs);
         // THEN
         expect(actual).toMatchObject(expected);
+    });
+});
+
+describe('表示用の残り時間文字列を組み立てられるか', () => {
+
+    test.each([
+        [
+            {minute: 25, second: 0}, '25:00'
+        ],
+        [
+            {minute: 8, second: 40}, '08:40',
+        ],
+        [
+            {minute: 14, second: 30}, '14:30',
+        ],
+    ])('分:秒の形式の残り時間を表示する文字列が組み立てられるか', (rest: RestTime, expected: string) => {
+        // GIVEN
+        const sut = toRestTimeString;
+        // WHEN
+        const actual = sut(rest);
+        // THEN
+        expect(actual).toBe(expected);
     });
 });
